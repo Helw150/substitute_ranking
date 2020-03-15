c4907e585bd4ea0aa5d3aa0c5d8c053bffcac70f
Helw150 <wbh230@nyu.edu>
2020-03-15 08:20:12 +0400

    Pip restructure

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-import setuptools
-from packagename.version import Version
+from setuptools import find_packages, setup
+from version import Version
 
 
-setuptools.setup(name='substitute_ranking',
+setup(name='substitute_ranking',
                  version=Version('0.0.1').number,
                  description='Lexical Substitution Ranking Package',
                  long_description=open('README.md').read().strip(),
                  author='William Held',
                  author_email='me@williamheld.com',
                  url='http://williamheld.com',
-                 py_modules=['substitute_ranking'],
+                 package_dir={"": "src"},
+                 packages=find_packages("src"),
                  install_requires=[],
                  license='MIT License',
                  zip_safe=False,
