81c546ed9f7c77296289c81f625849fbb1ca8535
Helw150 <wbh230@nyu.edu>
2020-03-15 09:07:36 +0400

    Handle Multi Occurrence Multi Token

--- src/substitute_ranking/bert_ranking.py
+++ src/substitute_ranking/bert_ranking.py
@@ -22,7 +22,7 @@ def self_attention_averages(attention, tokens, sub_token):
     attn_for_embedding = [attn[:, :, sub_token, j].mean() for j in range(0, len(tokens))]
     return attn_for_embedding
 
-def original_stats(input_sentence, target_word):
+def stats(input_sentence, target_word, attention_needed):
     token_subset = util.tokenize_word(target_word, tokenizer)
     inputs = tokenizer.encode_plus(input_sentence, return_tensors='pt', add_special_tokens=True)
     token_type_ids = inputs['token_type_ids']
@@ -31,28 +31,22 @@ def original_stats(input_sentence, target_word):
     input_id_list = input_ids[0].tolist() # Batch index 0
     tokens = tokenizer.convert_ids_to_tokens(input_id_list)
     range_of_target = util.find_sublist(token_subset, tokens)
-    sub_token = tokens.index(target_word)
-    return util.replace_with_average(embeddings[0], range_of_target), self_attention_averages(attention, tokens, sub_token)
+    if attention_needed:
+        sub_token = tokens.index(target_word)
+        return util.replace_with_average(embeddings[0], range_of_target), self_attention_averages(attention, tokens, sub_token)
+    else:
+        return util.replace_with_average(embeddings[0], range_of_target), None
 
-def substitution_stats(substitution_sentence, substitution_word):
-    token_subset = util.tokenize_word(substitution_word, tokenizer)
-    inputs = tokenizer.encode_plus(substitution_sentence, return_tensors='pt', add_special_tokens=True)
-    token_type_ids = inputs['token_type_ids']
-    input_ids = inputs['input_ids']
-    input_id_list = input_ids[0].tolist() # Batch index 0
-    tokens = tokenizer.convert_ids_to_tokens(input_id_list)
-    range_of_target = util.find_sublist(token_subset, tokens)
-    embeddings, _, _ = model(input_ids, token_type_ids=token_type_ids)
-    return util.replace_with_average(embeddings[0], range_of_target)
+
 
 def score_token(original_embedding, new_embedding, attention):
     return attention * cos(original_embedding, new_embedding)
     
 
 def score_substitution(input_sentence, original_word, substitution_word):
-    substitution_sentence = input_sentence.replace(original_word, substitution_word)
-    original_embeddings, attention = original_stats(input_sentence, original_word)
-    new_embeddings = substitution_stats(substitution_sentence, substitution_word)
+    substitution_sentence = input_sentence.replace(original_word, substitution_word, 1)
+    original_embeddings, attention = stats(input_sentence, original_word, True)
+    new_embeddings, _ = stats(substitution_sentence, substitution_word, False)
     assert(len(original_embeddings) == len(attention)), (len(original_embeddings), len(attention))
     assert(len(original_embeddings) == len(new_embeddings)), (len(original_embeddings), len(new_embeddings))
     return sum([score_token(original_embeddings[i], new_embeddings[i], attention[i]) for i in range(0, len(original_embeddings))]).item()
